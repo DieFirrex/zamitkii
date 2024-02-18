13b0d30e628714075308ff7b6a6e969b9452ac20
DieFirrex <154069304+DieFirrex@users.noreply.github.com>
2024-02-18 17:08:04 +0200

    Add files via upload

---
+++ ui1.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog 
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
+import json
+import os
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.add_note)
+        if os.path.exists("bobr.json") and os.path.getsize("bobr.json"):
+            with open("bobr.json", "r") as file:
+                self.notes = json.load(file)
+                self.ui.listWidget.addItems(self.notes.keys())
+
+    def add_note(self):
+        text, ok = QInputDialog.getText(self, 'Додати замітку', 'Введіть текст')
+        if ok:
+            self.ui.listWidget.addItem(text)
+
+
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
